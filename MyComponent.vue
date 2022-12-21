ba61cdd70e5f148b977fbfcead1ed6d42462c510
skr0nks <108427178+skr0nks@users.noreply.github.com>
2022-12-21 19:04:36 +0000

    Update MyComponent.vue

--- MyComponent.vue
+++ MyComponent.vue
@@ -123,3 +123,116 @@ export default {
 }
 
 </style> -->
+
+
+<!-- <template>
+<div class="user-card">
+  <h1>Научу зарабатывать 50к евро в год</h1>
+  <div class="main-inform">
+  <img class="pic1" src="../assets/crypto-riches-ethereum-shutterstock_786039952.jpg" alt="pic">
+ <h2><strong>{{ getAuthor }}</strong></h2>
+<p> Бизнес-коуч</p>
+<p>
+  Если этот феномен проходил и в прошлом столетии, тогда Солнце,
+  которое мы видим сейчас на 800 км меньше, чем то, которое видели
+  наши предки. Возможно, это солнечное сжатие является частью долговременного
+  колебания, которое помогает стабилизировать громадный выброс энергии.
+</p>
+      </div>
+  <p>Участников:{{ users.length }}</p>
+  <ul>
+    <li
+      v-for="(user,index) in users"
+      :key="index"
+      >
+      {{ index + ' '+getFullName(user)}}
+    </li>
+  </ul>
+
+<button
+type="button"
+@click="currentpage--"
+>Пред
+</button>
+<button
+v-for="page in pages"
+:key="page"
+type="button"
+@click="currentpage = page"
+>
+{{ page }}
+</button>
+<button
+type="button"
+@click="currentpage++"
+>
+След
+</button>
+
+<p>Страница {{ currentpage}} из {{ pages }}</p>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data () {
+    return {
+      firstName: 'Иван',
+      secondName: 'Иванов',
+      lastName: 'Иванович',
+      users: [
+        {
+          firstName: 'Илья',
+          secondName: 'Иванов',
+          lastName: 'Иванович'
+        },
+        {
+          firstName: 'Алекс',
+          secondName: 'Иванов',
+          lastName: 'Иванович'
+        },
+        {
+          firstName: 'Матвей',
+          secondName: 'Иванов',
+          lastName: 'Иванович'
+        }
+      ],
+      pages: 3,
+      currentpage: 1
+    }
+  },
+  computed: {
+    getAuthor () {
+      return this.firstName + ' ' + this.secondName
+    }
+  },
+  methods: {
+    getFullName (user) {
+      return user.firstName + ' ' + user.secondName
+    },
+    loadUsers (page) {
+      console.log('Загрузка пользователей : страница' + page)
+    }
+  },
+  watch: {
+    currentpage (newvalue, oldvalue) {
+      console.log('newvalue', newvalue)
+    }
+  }
+
+}
+</script>
+
+<style>
+
+big{
+  margin-left: 20px;
+}
+.pic1{
+  height: 200px;
+  width: 300px;
+}
+</style> -->
+
